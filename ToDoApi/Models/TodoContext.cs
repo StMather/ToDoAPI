53454c6dda95eb4548836c76b2c4ad033226e068
Steven Mather <stevenrmather@gmail.com>
2021-10-22 09:48:41 -0700

    Adding the TodoContext database context

---
+++ ToDoApi/Models/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> optiond) : base(options)
+        {
+
+        }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+    }
+}
